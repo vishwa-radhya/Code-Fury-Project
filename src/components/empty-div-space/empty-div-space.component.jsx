ba1239f1b18ca44899562d0cd9091fcb769e33d3
Manju18174 <manjunathreddy18174@gmail.com>
2024-08-10 22:36:31 +0530

    finished setup landing page also added fa icons

---
+++ src/components/empty-div-space/empty-div-space.component.jsx
@@ -0,0 +1,14 @@
+import './empty-div-space.styles.scss'
+
+const EmptyDivSpace=()=>{
+    return(
+        <div className="empty-space-div">
+            <i className="fa-regular fa-snowflake"></i>
+            <i className="fa-solid fa-tornado"></i>
+            <i className="fa-solid fa-hurricane"></i>
+            <i className="fa-solid fa-hill-rockslide"></i>
+            <i className="fa-solid fa-cloud-bolt"></i>
+        </div>
+    )
+}
+export default EmptyDivSpace;
