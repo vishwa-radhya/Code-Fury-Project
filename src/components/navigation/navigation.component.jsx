20df09b1a811a4ee9eb9f2a7bbfbe66473dc443f
vishwa-radhya <vishuvishwa735@gmail.com>
2024-08-10 15:32:56 +0530

    added auth component

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,16 @@
+import './navigation.styles.scss';
+const Navigation=()=>{
+    return(
+        <div className="navigation-bar">
+            <div>
+                <p>navigation bar</p>
+            </div>
+            <div className='nav-bar-items'>
+                <li>Live Disasters</li>
+                <li>Precautions</li>
+                <li>Helpline</li>
+            </div>
+        </div>
+    )
+}
+export default Navigation;
