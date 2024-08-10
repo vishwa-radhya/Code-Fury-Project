d5df43cc421e6ce82299d10dc9feea27916d21e8
Manju18174 <manjunathreddy18174@gmail.com>
2024-08-10 16:13:55 +0530

    added firebase setup with google sign in interation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css'
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import { auth } from './utils/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 // import Navigation from './components/navigation/navigation.component'
 import Auth from './components/auth/auth.component'
-function App() {
+import Container from './components/container/container.component';
 
+function App() {
+  const [user,setUser]=useState(null);
+
+  useEffect(()=>{
+    const checkAuthState=async()=>{
+      const unsubscribe = onAuthStateChanged(auth,(user)=>{
+        
+        if(user){
+          setUser(user);
+        }else{
+          setUser(null);
+        }
+      })
+      return ()=> unsubscribe();
+    }
+    checkAuthState();
+  },[user])
+  
   return (
     <Fragment>
-      <Auth/>
+      {user ? <Container/> : <Auth/>}
     </Fragment>
   )
 }
